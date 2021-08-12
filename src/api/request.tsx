f267cfb423cede70a08bf57819ad455cf593b845
fjij <30779570+fjij@users.noreply.github.com>
2021-08-11 21:00:39 -0400

    Fix api path concatenation

--- src/api/request.tsx
+++ src/api/request.tsx
@@ -1,18 +1,20 @@
 import axios, {AxiosRequestConfig} from 'axios';
+import path from 'path';
+
 const baseUrl = process.env.REACT_APP_API_URL;
 
 const request = {
   "get": (url: string, config: AxiosRequestConfig = {}) => {
-    return axios.get(new URL(url, baseUrl).href, config);
+    return axios.get(path.join(baseUrl??'', url), config);
   },
   "post": (url: string, data: any = null, config: AxiosRequestConfig = {}) => {
-    return axios.post(new URL(url, baseUrl).href, data, config);
+    return axios.post(path.join(baseUrl??'', url), data, config);
   },
   "put": (url: string, data: any = null, config: AxiosRequestConfig = {}) => {
-    return axios.put(new URL(url, baseUrl).href, data, config);
+    return axios.put(path.join(baseUrl??'', url), data, config);
   },
   "delete": (url: string, config: AxiosRequestConfig = {}) => {
-    return axios.delete(new URL(url, baseUrl).href, config);
+    return axios.delete(path.join(baseUrl??'', url), config);
   }
 };
 
